875d0c1dde090a4fd9b0ab200f808aa30c57c3cb
Juan José <jujopere@bancolombia.com.co>
2025-08-21 11:17:15 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://juanp88.github.io',
-
+    site: 'https://juanp88.github.io/',
+    outDir: './dist',
+    output: 'static'
 });
