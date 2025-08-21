f3eac08cdd2586b12524de8a53061e57b27b7436
Juan José <jujopere@bancolombia.com.co>
2025-08-21 08:33:40 -0500

    remove duplicated comma

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://juanjose.github.io',,
+    site: 'https://juanjose.github.io',
 });
